c6951f89c5f431862ca2e6fa9e60dcfec28a2888
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-01-18 23:56:43 +0200

    parsers

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mysql from 'mysql2';
 import cors from 'cors';
 import bcrypt, {hash} from 'bcrypt';
-
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const saltRounds  = 3;
 
@@ -10,7 +11,17 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json())
-app.use(cors())
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+        credentials: true,
+    })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 const db = mysql.createConnection({
     user:'root',
